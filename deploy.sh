94248e5adbda887b4d01bfd5b3130fd8f60136f8
noahnefsky <77079369+noahnefsky@users.noreply.github.com>
2021-02-17 16:39:09 -0500

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-yarn build
-find build -type f -exec gzip "{}" \; -exec mv "{}.gz" "{}" \;
-aws s3 cp --recursive --acl public-read --content-encoding gzip --cache-control max-age=60 build s3://noahnefsky.com
